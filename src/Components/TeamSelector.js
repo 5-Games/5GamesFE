1e896f856e0422391eb85eecb6abd74c109ea826
github username <github email address>
2020-03-30 13:31:20 -0400

    deleted white space

--- src/Components/TeamSelector.js
+++ src/Components/TeamSelector.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import actions from '../redux/actions.js';
 
-
 const TeamSelector = () => {
 
   // initializing dispatch
