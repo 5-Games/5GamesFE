dcbc87a6ab3b8152ffc632211eb9dfc8a203c98e
Stephen Keating <stephen.keating@gmail.com>
2020-04-17 17:47:27 -0400

    started formatting of playlist page

--- src/Pages/Playlist.js
+++ src/Pages/Playlist.js
@@ -7,19 +7,42 @@ const Playlist = ({ playlistId }) => {
   // const dispatch = useDispatch();
   const [playlist, setPlaylist] = useState({});
 
+  console.log(playlist)
+
   useEffect(() => {
-    // dispatch(actions.getCurrentPlaylist(playlistId))
-    // .then(data => setPlaylist(data))
     fetch('http://localhost:3000/playlists/' + playlistId)
       .then(r => r.json())
       .then(res => setPlaylist(res))
   }, []);
 
-  return (
-    <div>
-      {playlist.title}
-    </div>
-  );
+  const description = playlist.description
+    ?<h2>
+      {playlist.description}
+    </h2>
+    :null
+
+  return <div className="game-show-page">
+            <h1>
+              {playlist.title}
+            </h1>
+              {description}
+            <div className='row'>
+              <div className='single-column'>
+                <div className='floating-div'>
+                  <div className="sub-row"> 
+                    left column
+                    {/* {editPlaylistGamesMap()} */}
+                  </div>
+                </div>
+              </div>
+              <div className='double-column'>
+                <div className="sub-row">
+                  right column
+                  {/* {editGamesMap()} */}
+                </div>
+              </div>
+            </div>
+        </div>
 };
 
 export default Playlist;
