50af2b9b68825ee5837ecf85286df76c79ed65a5
Stephen Keating <stephen.keating@gmail.com>
2020-04-22 08:55:02 -0400

    changed new date in redux to try to fix safari bug

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -14,7 +14,7 @@ const initialState = {
   selectGames: {
     // When NBA games resume, change the value of date to today
     // date: new Date('March 11, 2020 00:00:00'),
-    date: new Date(1583928000),
+    date: new Date('Wednesday, 11-Mar-20 00:00:00 UTC'),
     games: [],
     method: 'date',
     team: '1',
